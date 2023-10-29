3088dc44eadb351032f45a0b1b000492834ae944
unknown <deepikakendole07@gmail.com>
2023-10-29 19:54:40 +0530

    first commit

---
+++ pick.js
@@ -0,0 +1,22 @@
+let containerE1 = document.getElementById("colorPickerContainer");
+let spanE1 = document.getElementById("selectedColorHexCode");
+
+function greyBtn(){
+    containerE1.style.backgroundColor = "#e0e0e0";
+    spanE1.textContent = "#e0e0e0";
+}
+
+function greenBtn(){
+    containerE1.style.backgroundColor = "#6fcf97";
+    spanE1.textContent = "#6fcf97";
+}
+
+function blueBtn(){
+    containerE1.style.backgroundColor = "#56ccf2";
+    spanE1.textContent = "#56ccf2";
+} 
+
+function violetBtn(){
+    containerE1.style.backgroundColor = "#bb6bd9";
+    spanE1.textContent = "#bb6bd9";
+} 
